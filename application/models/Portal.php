a1a791cab1879838038f7a54ded48eec5d89a081
Yuzo <hyzgog@gmail.com>
2015-03-04 11:32:59 +0800

    add keyword

--- application/models/Portal.php
+++ application/models/Portal.php
@@ -13,34 +13,50 @@ class Portal extends CI_Model {
 	 * Retrive all info
 	 *
 	 * This method is quite costy, need cache
-	 * @return  array
+	 * @return  mixed
 	 */
 	public function query()
 	{
 		$data = array();
+		$keyword = array();
 
 		$category = array();
 		$sql = 'SELECT `category`.`ID`, `category`.`name`, `category`.`info`, `category_field`.`name` AS `field` FROM `category` LEFT JOIN `category_field` ON `category`.`ID` = `category_field`.`categoryID` ORDER BY `category_field`.`rank` ASC';
 		foreach ($this->db->query($sql)->result_array() as $row) {
-			if (isset($category[$row['ID']])) {
-				$category[$row['ID']]['field'][] = $row['field'];
+			if (isset($category[$row['name']])) {
+				$category[$row['name']]['field'][] = $row['field'];
 			} else {
-				$ID = $row['ID'];
-				unset($row['ID']);
+				$name = $row['name'];
+				unset($row['name']);
 
 				if (isset($row['field'])) {
+					// Push field(may be duplicate) to keyword
+					if (!isset($keyword[$row['field']])) {
+						$keyword[$row['field']] = array(
+							'type' => 'field'
+						);
+					}
+					// End keyword
+
 					$row['field'] = array($row['field']);
 				} else {
 					$row['field'] = array();
 				}
 
-				$category[$ID] = $row;
+				$category[$name] = $row;
+
+				// Push category(never duplicate) to keyword
+				$keyword[$name] = array(
+					'type' => 'category',
+					'ID' => $row['ID']
+				);
+				// End keyword
 			}
 		}
 		$data['category'] = $category;
 
 		$group = array();
-		$sql = 'SELECT `categoryID`, `name`, `rank` FROM `category_keyword`';
+		$sql = 'SELECT `category_keyword`.`name`, `category_keyword`.`rank`, `category`.`name` AS `category`, `category`.`ID` FROM `category_keyword` JOIN `category` ON `category_keyword`.`categoryID` = `category`.`ID`';
 		foreach ($this->db->query($sql)->result_array() as $row) {
 			if(!isset($group[$row['rank']])) {
 				$group[$row['rank']] = array();
@@ -48,12 +64,21 @@ class Portal extends CI_Model {
 
 			if(!isset($group[$row['rank']][$row['name']])) {
 				$group[$row['rank']][$row['name']] = array();
+
+				// Push group(never duplicate) to keyword
+				$keyword[$row['name']] = array(
+					'type' => 'group',
+					'ID' => $row['ID']
+				);
+				// End keyword
 			}
 
-			$group[$row['rank']][$row['name']][] = $row['categoryID'];
+			$group[$row['rank']][$row['name']][] = $row['category'];
 		}
 
 		$data['group'] = $group;
+
+		$data['keyword'] = $keyword;
 		return $data;
 	}
 
