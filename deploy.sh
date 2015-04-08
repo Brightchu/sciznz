415296c141648708caa239a194a2a7661cef6849
Yuzo <hyzgog@gmail.com>
2015-04-09 00:58:05 +0800

    mv jade to front

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd /tmp/sciclubs/
 
 # compile
 coffee --join static/js/admin.js --compile static/js/admin/app.coffee static/js/admin/controller.coffee static/js/admin/service.coffee
-coffee --join static/js/script.js --compile static/js/front/app.coffee static/js/front/controller.coffee static/js/front/service.coffee static/js/front/filter.coffee
+coffee --join static/js/front.js --compile static/js/front/app.coffee static/js/front/controller.coffee static/js/front/service.coffee static/js/front/filter.coffee
 coffee --join static/js/ucenter.js --compile static/js/ucenter/app.coffee static/js/ucenter/controller.coffee static/js/ucenter/service.coffee static/js/ucenter/filter.coffee
 coffee --join static/js/supply.js --compile static/js/supply/app.coffee static/js/supply/controller.coffee static/js/supply/service.coffee static/js/supply/filter.coffee
 coffee --join static/js/group.js --compile static/js/group/app.coffee static/js/group/controller.coffee static/js/group/service.coffee static/js/group/filter.coffee
@@ -20,7 +20,7 @@ lessc -x static/css/group.less static/css/group.css
 
 jade application/views/*.jade
 jade application/views/mail/*.jade&
-jade static/partial/*.jade
+jade static/partial/front/*.jade
 jade static/partial/admin/*.jade
 jade static/partial/ucenter/*.jade
 jade static/partial/supply/*.jade
@@ -28,7 +28,7 @@ jade static/partial/group/*.jade
 
 # compress
 uglifyjs static/js/admin.js --mangle --compress --screw-ie8 -o static/js/admin.js
-uglifyjs static/js/script.js --mangle --compress --screw-ie8 -o static/js/script.js
+uglifyjs static/js/front.js --mangle --compress --screw-ie8 -o static/js/front.js
 uglifyjs static/js/ucenter.js --mangle --compress --screw-ie8 -o static/js/ucenter.js
 uglifyjs static/js/supply.js --mangle --compress --screw-ie8 -o static/js/supply.js
 uglifyjs static/js/group.js --mangle --compress --screw-ie8 -o static/js/group.js
--- development.sh
+++ development.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 coffee --join static/js/admin.js --watch --compile static/js/admin/app.coffee static/js/admin/controller.coffee static/js/admin/service.coffee&
-coffee --join static/js/script.js --watch --compile static/js/front/app.coffee static/js/front/controller.coffee static/js/front/service.coffee static/js/front/filter.coffee&
+coffee --join static/js/front.js --watch --compile static/js/front/app.coffee static/js/front/controller.coffee static/js/front/service.coffee static/js/front/filter.coffee&
 coffee --join static/js/ucenter.js --watch --compile static/js/ucenter/app.coffee static/js/ucenter/controller.coffee static/js/ucenter/service.coffee static/js/ucenter/filter.coffee&
 coffee --join static/js/supply.js --watch --compile static/js/supply/app.coffee static/js/supply/controller.coffee static/js/supply/service.coffee static/js/supply/filter.coffee&
 coffee --join static/js/group.js --watch --compile static/js/group/app.coffee static/js/group/controller.coffee static/js/group/service.coffee static/js/group/filter.coffee&
 
 jade -w -P application/views/*.jade&
 jade -w -P application/views/mail/*.jade&
-jade -w -P static/partial/*.jade&
+jade -w -P static/partial/front/*.jade&
 jade -w -P static/partial/admin/*.jade&
 jade -w -P static/partial/ucenter/*.jade&
 jade -w -P static/partial/supply/*.jade&
