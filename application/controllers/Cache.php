d5c33939345627c2535b4a0a149f7b0a85314f95
Yuzo <hyzgog@gmail.com>
2015-04-08 13:29:34 +0800

    rename cache

--- application/controllers/Cache.php
+++ application/controllers/Cache.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cache extends CI_Controller {
 
-	public function query()
+	public function index()
 	{
-		$this->load->model('cache');
-		$cache_date = $this->cache->query_date();
+		$this->load->model('cache_model');
+		$cache_date = $this->cache_model->query_date();
 
 		if ($cache_date === $this->input->get_request_header('If-Modified-Since')) {
 			$this->output->set_status_header(304);
@@ -14,82 +14,8 @@ class Cache extends CI_Controller {
 			$this->output->set_header('Cache-Control: public');
 			$this->output->set_header('Last-Modified: ' . $cache_date);
 			$this->output->set_content_type('application/json');
-			$this->output->set_output($this->cache->query());
+			$this->output->set_output($this->cache_model->query());
 		}
 	}
 
-	public function user()
-	{
-		$this->load->model('user');
-		$this->load->library('kvdb');
-		$this->load->helper('cookie');
-
-		switch ($this->input->method()) {
-			case 'put':
-				$result = $this->user->login($this->input->json('email'), $this->input->json('password'));
-				if ($result) {
-					$result['timestamp'] = time();
-
-					$value = json_encode($result, JSON_NUMERIC_CHECK);
-					$key = sha1($value);
-					$this->kvdb->set('session_' . $key, $value);
-
-					set_cookie('session', $key, 31536000);
-					set_cookie('name', $result['name'], 31536000);
-					$this->output->set_status_header(200);
-				} else {
-					$this->output->set_status_header(403);
-				}
-				break;
-
-			case 'post':
-				$row = $this->input->json();
-				$row['name'] = $row['email'];
-				$row['credit'] = 0;
-				$row['phone'] = '';
-
-				$result = $this->user->save($row);
-				if ($result) {
-					$result = array(
-						'ID' => $this->user->db->insert_id(),
-						'name' => $row['name'],
-						'credit' => $row['credit'],
-						'timestamp' => time(),
-					);
-
-					$value = json_encode($result, JSON_NUMERIC_CHECK);
-					$key = sha1($value);
-					$this->kvdb->set('session_' . $key, $value);
-
-					set_cookie('session', $key, 31536000);
-					set_cookie('name', $result['name'], 31536000);
-					$this->output->set_status_header(200);
-				} else {
-					$this->output->set_status_header(403);
-				}
-				break;
-		}
-	}
-
-	public function order()
-	{
-		$this->load->model('order');
-		$this->load->library('kvdb');
-
-		switch ($this->input->method()) {
-			case 'get':
-				$deviceID = $this->input->get('deviceID');
-				$useDate = $this->input->get('useDate');
-				$this->output->set_json($this->order->count($deviceID, $useDate));
-				break;
-
-			case 'post':
-				$session = json_decode($this->kvdb->get('session_' . $this->input->cookie('session')), TRUE);
-				$req = $this->input->json();
-				$req['userID'] = $session['ID'];
-				$result = $this->order->book($req);
-				$this->output->set_status_header($result ? 200 : 403);
-				break;
-		}
-	}
 }
